9d89c9908cc27d2f3941469070db58817857c128
crisvar420 <crisrv098@gmail.com>
2023-03-01 23:23:03 -0600

    ros2_control update

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,14 +2,11 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
-
-
 
 def generate_launch_description():
 
@@ -38,10 +35,23 @@ def generate_launch_description():
                         output='screen')
 
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
